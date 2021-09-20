9379d906074eba0c67b53d83b34ee2415a77407f
Jdrew92 <darkkhir0@gmail.com>
2021-09-20 09:17:05 -0500

    Se crean los proyectos de dominio, persistencia, servicios y consola

---
+++ NutricionApp/NutricionApp.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NutricionApp.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ NutricionApp/NutricionApp.Persistencia/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NutricionApp.Persistencia
+{
+    public class Class1
+    {
+    }
+}
