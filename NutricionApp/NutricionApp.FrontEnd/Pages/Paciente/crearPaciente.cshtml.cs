11068ef04b989a50a83eb9758918948e39bf6810
Jorge A. Ayala <Darkkhir0@gmail.com>
2021-10-07 16:32:01 -0500

    add Referencias y organizacion de front Paciente

---
+++ NutricionApp/NutricionApp.FrontEnd/Pages/Paciente/crearPaciente.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NutricionApp.Persistencia;
+using NutricionApp.Dominio;
+
+namespace NutricionApp.FrontEnd.Pages
+{
+    public class crearPacienteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
